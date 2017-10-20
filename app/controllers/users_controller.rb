98838b6587afd21b35b9404010423cc6542edfb9
Jacob Bijani <jacob.bijani@gmail.com>
2017-10-20 15:14:47 -0400

    dont require auth for create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user, except: [:show]
+  before_action :authenticate_user, except: [:show, :create]
 
   def show
     render json: User.find_by(username: params[:id])
