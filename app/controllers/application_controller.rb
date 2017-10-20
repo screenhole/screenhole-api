4d7474bd71faa32c96a0c629d69db4bbf64572d5
Jacob Bijani <jacob.bijani@gmail.com>
2017-10-20 10:45:20 -0400

    if the request includes a bearer header, automatically include a refreshed token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::API
   include Knock::Authenticable
 
+  before_action :refresh_token, if: :check_auth_header
+
   def pagination_dict(collection)
     {
       current_page: collection.current_page,
@@ -10,4 +12,15 @@ class ApplicationController < ActionController::API
       total_count: collection.total_count
     }
   end
+
+  private
+
+    def check_auth_header
+      request.env["HTTP_AUTHORIZATION"] =~ /Bearer/
+    end
+
+    def refresh_token
+      authenticate_user
+      headers['Authorization'] = Knock::AuthToken.new(payload: { sub: current_user.id }).token
+    end
 end
