222a217c6bfc18629cf7050c29eb6db70ed59770
Jacob Bijani <jake@users.noreply.github.com>
2017-12-13 20:55:47 -0500

    try to fix lib issue
    https://stackoverflow.com/questions/17007685/rails-4-uninitialized-constant-for-module
    https://collectiveidea.com/blog/archives/2016/07/22/solutions-to-potential-upgrade-problems-in-rails-5

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,7 @@ module ScreenholeApi
     # -- all .rb files in that directory are automatically loaded.
 
     config.autoload_paths += %W(#{config.root}/lib)
+    config.eager_load_paths += %W(#{config.root}/lib)
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
