587cb9eb5dfeb143005c316b2feb5ca7492e8d07
Jacob Bijani <jacob.bijani@gmail.com>
2017-12-20 12:54:11 -0500

    AddIndexToUsernames

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219215907) do
+ActiveRecord::Schema.define(version: 20171220175315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 20171219215907) do
     t.string "name"
     t.string "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
