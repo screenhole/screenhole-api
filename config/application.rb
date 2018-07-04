e71560944988bb5808864533b9cbcd4c1fc3e230
Alex Kluew <dropbyalex@gmail.com>
2018-07-04 08:52:21 -0400

    Fix: add config.action_cable.mount_path

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,6 @@ module ScreenholeApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.action_cable.mount_path = '/cable/:token'
   end
 end
