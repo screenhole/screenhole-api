c083eb12f19123b82689b51cbdd5579bd3380562
Josh McMillan <josh@joshmcmillan.co.uk>
2019-02-08 03:10:43 +0000

    Add a countries endpoint
    This provides a list of country codes that can be used in the UI when
    selecting a country.

---
+++ app/controllers/countries_controller.rb
@@ -0,0 +1,15 @@
+class CountriesController < ApplicationController
+  before_action :authenticate_user
+
+  def index
+    render json: {
+      countries: ISO3166::Country.all.map do |c|
+        {
+          name: c.name,
+          code: c.alpha2,
+          emoji: c.emoji_flag
+        }
+      end
+    }
+  end
+end
