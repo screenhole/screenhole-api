126cd3546431539b8317b1ec32b07fb3657974d5
Josh McMillan <josh@joshmcmillan.co.uk>
2019-04-01 18:44:38 +0100

    Remove unused test files

--- test/models/buttcoin_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ButtcoinTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/chomment_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ChommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
